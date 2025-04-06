b22ca52943c602dfd8b06383107829c7e22549ac
aswathi-48 <aswathik482@gmail.com>
2025-04-06 20:30:27 +0530

    form message send to mail

---
+++ scrip.js
@@ -0,0 +1,27 @@
+// function emailSend(){
+//     Email.send({
+//         Host : "s1.maildns.net",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+
+// function sendMail(){
+//    let parms ={
+//       name: document.getElementById("fname").value,
+//       email: document.getElementById("email").value,
+//       country: document.getElementById("country").value,
+//       subject: document.getElementById("subject").value,
+//       message: document.getElementById("message").value
+
+
+//    }
+//    emailjs.send("service_0no4i7g","template_neg2z3m",parms).then(alert("Email has send"))
+// }
